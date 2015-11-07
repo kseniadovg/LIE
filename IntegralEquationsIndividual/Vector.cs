8ee6207ff1182ad0f47eca550b63c0ef5f37d905
OksanaDovhanyk <ksenia_dovg@ukr.net>
2015-11-07 16:20:53 +0200

    create function from string

---
+++ IntegralEquationsIndividual/Vector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegralEquationsIndividual
+{
+    class Vector<T>
+    {
+        public T a { get; private set; }
+        public T b { get; private set; }
+
+        public Vector(T a, T b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
